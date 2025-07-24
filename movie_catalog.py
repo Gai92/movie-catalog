4dcd22712d4f9132908aae80de12a440248842f0
Liliia Gainutdinova <rahmatullinalr92@gmail.com>
2025-07-24 22:47:23 +0930

    Returning all movies in the catalog

--- movie_catalog.py
+++ movie_catalog.py
@@ -1,7 +1,30 @@
 class MovieCatalog:
+    VALID_GENRES = ["drama", "comedy", "sci-fi", "action", "horror"]
+
     def __init__(self):
         self.movies = []
 
+    # Return all movies
+    def get_all_movies(self):
+        if not self.movies:
+            print("The catalog is currently empty.")
+        return self.movies
+
 def main():
     catalog = MovieCatalog()
-    print("Movie Catalog App")
+
+    # Temporary demo: manually pre-fill movies
+    catalog.movies = [
+        {"title": "Inception", "genre": "sci-fi"},
+        {"title": "The Notebook", "genre": "drama"},
+        {"title": "Interstellar", "genre": "sci-fi"}
+    ]
+    #Print all movies
+    print("\nAll Movies in Catalog:")
+    movies = catalog.get_all_movies()
+    for movie in movies:
+        print(f" {movie['title']} ({movie['genre']})")
+
+
+if __name__ == "__main__":
+    main()
